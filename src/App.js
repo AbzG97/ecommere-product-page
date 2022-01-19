5661d706f31ab27ffdae8fcab26f103dcade5b32
AbzG97 <Abdulla.ghazalah@hotmail.com>
2022-01-18 23:04:08 -0500

    added more items to test

--- src/App.js
+++ src/App.js
@@ -8,18 +8,40 @@ import MenPage from './Components/MenPage';
 import WomenPage from './Components/WomenPage';
 import AboutPage from './Components/AboutPage';
 import ContactPage from './Components/ContactPage';
+import {v4 as uuid} from 'uuid';
+import image_product_1 from './images/image-product-1.jpg'
 
 
 
 function App() {
   const [item_in_cart, setItem_in_cart] = React.useState([]);
+  const [items, setItems] = React.useState([
+    {
+      "id": uuid(),
+      "product_title": "Fall Limited Edition Sneakers",
+      "product_price_after_sale": 125.00,
+      "image": image_product_1
+   },
+   {
+    "id": uuid(),
+    "product_title": "Fall Limited Edition Sneakers",
+    "product_price_after_sale": 125.00,
+    "image": image_product_1
+ },
+ {
+  "id": uuid(),
+  "product_title": "Fall Limited Edition Sneakers",
+  "product_price_after_sale": 125.00,
+  "image": image_product_1
+},
+  ])
 
   return (
     <div className="App">
       <Navbar item_in_cart={item_in_cart} setItem_in_cart={setItem_in_cart}/>
       <Routes>
         <Route path="/" element={<Item item_in_cart={item_in_cart} setItem_in_cart={setItem_in_cart}/>}/>
-        <Route path="/collections" element={ <CollectionsPage/>}/>
+        <Route path="/collections" element={ <CollectionsPage items={items}/>}/>
         <Route path="/men" element={ <MenPage/>}/>
         <Route path="/women" element={ <WomenPage/>}/>
         <Route path="/about" element={ <AboutPage/>}/>
--- src/Components/CollectionsPage.js
+++ src/Components/CollectionsPage.js
@@ -1,10 +1,9 @@
 import React from 'react'
+import Item from './Item'
 
-function CollectionsPage() {
+function CollectionsPage({items}) {
     return (
         <div>
-            <h1>Collections Page</h1>
-            
         </div>
     )
 }
